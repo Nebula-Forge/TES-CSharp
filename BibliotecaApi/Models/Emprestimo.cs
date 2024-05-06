134a82fd31e58650fdbaa367c77b9c4e7451dce6
Victor Martins <VictorMLMartins@icloud.com>
2024-05-06 13:04:04 -0300

    feat: :sparkles: Classe modelo "Empréstimo"
    Realizado a criação da classe modelo "Empréstimo", Getter's, Setter's, Contructors e Relacionamento

--- BibliotecaApi/Models/Emprestimo.cs
+++ BibliotecaApi/Models/Emprestimo.cs
@@ -1,3 +1,26 @@
 using System.ComponentModel.DataAnnotations;
+namespace BiblotecaApi.Models;
+public class Emprestimo
+{
+  public Emprestimo() { }
+  public Emprestimo(Usuario usuario, List<Livro> livros)
+  {
+    Id = Guid.NewGuid().ToString();
+    Usuario = usuario;
+    Livros = livros;
+    DataEmprestimo = DateTime.Now;
+    DataDevolucaoPrevista = DateTime.Now.AddDays(7);
+    Status = "Em andamento";
+  }
 
-namespace API.Models;
+  public string? Id { get; private set; }
+  public DateTime DataEmprestimo { get; private set; }
+  public DateTime DataDevolucaoPrevista { get; private set; }
+  public DateTime? DataDevolucaoReal { get; private set; }
+  public string Status { get; private set; }
+
+  public Usuario? Usuario { get; private set; }
+  public string? UsuarioId { get; private set; }
+
+  public virtual List<Livro>? Livros { get; private set; }
+}
