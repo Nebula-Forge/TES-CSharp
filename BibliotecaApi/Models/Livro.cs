0456f87839a42d3b0b6ca1ba18418b99bda33e94
danilomf96 <121731610+danilomf96@users.noreply.github.com>
2024-05-11 15:30:26 -0300

    Classe Livro

--- BibliotecaApi/Models/Livro.cs
+++ BibliotecaApi/Models/Livro.cs
@@ -1,8 +1,24 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace API.Models;
-
-public class Livro
+namespace API.Models
 {
-    
+    public class Livro
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O título do livro é obrigatório.")]
+        public string? Titulo { get; set; }
+
+        [Required(ErrorMessage = "O autor do livro é obrigatório.")]
+        public string? Autor { get; set; }
+
+        [Required(ErrorMessage = "O ISBN do livro é obrigatório.")]
+        public string? ISBN { get; set; }
+
+        public string? Editora { get; set; }
+
+        public int AnoPublicacao { get; set; }
+
+    }
 }
